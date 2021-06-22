aeea6839c3bc7dbc550a5ca2faeb97f1bcb57180
kevinomyonga <komyonga@gmail.com>
2021-06-22 19:57:44 +0300

    Genesis
    Initialised the application to use the Spring Java framework.

---
+++ src/test/java/com/kevinomyonga/kyoskbackendtest/KyoskBackendTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinomyonga.kyoskbackendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyoskBackendTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
